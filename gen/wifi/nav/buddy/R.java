78b560b77727ceb1dc3191374a95fdc4bb45d32b
Aaron Horn <superboringturtle@hotmail.com>
2013-01-23 17:15:18 -0600

    fixed R.java error

--- gen/wifi/nav/buddy/R.java
+++ gen/wifi/nav/buddy/R.java
@@ -16,7 +16,8 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int dataText=0x7f070001;
+        public static final int menu_settings=0x7f070002;
     }
     public static final class layout {
         public static final int activity_scan_view=0x7f030000;
